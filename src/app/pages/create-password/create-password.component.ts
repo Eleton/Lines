85e3425dd95af6ac185b99402b132ebe39a4d6d3
Aiman Josefsson <aiman@wec360.se>
2018-10-21 17:12:00 +0200

    Buttons work in password-view

--- src/app/pages/create-password/create-password.component.ts
+++ src/app/pages/create-password/create-password.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePasswordComponent implements OnInit {
 
+  password = '';
+  passwordArray = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.passwordConverter();
+  }
+
+  inputDigit(digit: string) {
+    if (this.password.length < 4) {
+      this.password = this.password + digit;
+    }
+    this.passwordConverter();
+  }
+
+  deleteDigit() {
+    if (this.password.length > 0) {
+      this.password = this.password.slice(0, this.password.length - 1);
+    }
+    this.passwordConverter();
+  }
+
+  private passwordConverter() {
+    const passArr = this.password.split('');
+    while (passArr.length < 4) {
+      passArr.push('');
+    }
+    this.passwordArray = passArr;
   }
 
 }
